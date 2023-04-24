22322885a87e24a3eda9f68dff42140ae5d8ff8c
enriqueaquije <bryanaquije@gmail.com>
2023-04-24 16:24:35 -0500

    new lines in console.js

--- javascript/console.js
+++ javascript/console.js
@@ -6,5 +6,5 @@ esto es solo un comentario
 /*hola papus*/
 console.log("Happy Birthday number 26");
 console.log("hello x8000");
-
-console.log("24/04/2023");
+let name = "Enrique";
+console.log(`Hola ${name} el día de hoy irás al cine`);
